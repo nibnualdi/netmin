e36b11c8c012760c8d7a5c696016ced0fe036bf5
nibnualdi <nibnualdi@gmail.com>
2022-05-18 20:39:29 +0700

    fix bug canot get in when the user name is more than 2 words

--- chatting-app/src/PrivateRoute.js
+++ chatting-app/src/PrivateRoute.js
@@ -4,8 +4,8 @@ import { Navigate, Outlet } from "react-router-dom";
 const PrivateRoute = () => {
   const auth = Cookies.get("auth");
 
-  const url = window.location.href.split("home:")[1]
-  let user = url.includes("%20") ? url.replace("%20", " ") : url
+  const url = window.location.href.split("home:")[1];
+  let user = url.includes("%20") ? url.replace(/%20/g, " ") : url;
 
   return <>{auth === user ? <Outlet /> : <Navigate to="/" />}</>;
 };
